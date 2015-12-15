ff94f13464c450d6f1f992456dbe351a0da67ba9
suner <yufeng.sun@autodesk.com>
2015-12-15 17:57:01 +0800

    spotify-recommend

---
+++ spotify-recommend/server.js
@@ -0,0 +1,50 @@
+var unirest = require('unirest');
+var express = require('express');
+var events = require('events');
+
+var getFromApi = function(endpoint, args) {
+    var emitter = new events.EventEmitter();
+    unirest.get('https://api.spotify.com/v1/' + endpoint)
+           .qs(args)
+           .end(function(response) {
+                if (response.ok) {
+                    emitter.emit('end', response.body);
+                }
+                else {
+                    emitter.emit('error', response.code);
+                }
+            });
+    return emitter;
+};
+
+var app = express();
+app.use(express.static('public'));
+
+app.get('/search/:name', function(req, res) {
+    var searchReq = getFromApi('search', {
+        q: req.params.name,
+        limit: 1,
+        type: 'artist'
+    });
+
+    searchReq.on('end', function(item) {
+        var artist = item.artists.items[0];
+		console.log(artist.id);
+		
+		// find related artists
+        var relatedArtistsReq = getFromApi('artists/'+artist.id+'/related-artists');
+		relatedArtistsReq.on('end', function(item) {
+			artist.related = item.artists;
+			res.json(artist);
+		});
+		relatedArtistsReq.on('error', function(code) {
+			res.sendStatus(code);
+		});
+    });
+
+    searchReq.on('error', function(code) {
+        res.sendStatus(code);
+    });
+});
+
+app.listen(8080);
